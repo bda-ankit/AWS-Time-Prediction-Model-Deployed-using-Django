26b9721ffe6d7444d16ed356f6eee1aa4c5054bd
hackers0007 <ankitparmar6101@gmail.com>
2024-02-20 09:18:23 +0530

    commit all

--- deployment/views.py
+++ deployment/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 import joblib
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def result(request):
     cls = joblib.load('linear_regression_model.joblib')
