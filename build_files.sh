d2a6d54256bd3068bf11c5e9c339c094da984811
hackers0007 <ankitparmar6101@gmail.com>
2024-02-20 09:37:46 +0530

    requirements

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "Build start"
-python3.9 -m pip install -r requirements.text
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build end"
